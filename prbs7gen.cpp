ea7097c82123670ab070760608775bb5cf564178
gram117 <gdanderson9@gmail.com>
2025-06-30 10:08:44 -0700

    updated prbs7 gen to work with both sequences and updated file output

--- prbs7gen.cpp
+++ prbs7gen.cpp
@@ -3,22 +3,27 @@
 #include <vector>
 #include <iomanip>
 #include <fstream>
+#include <filesystem>
 
 static const int SEQ_SIZE = 127 * 3;
 
 
 int main() {
-    std::ofstream outputFile("prbs7.txt");
-    if (!outputFile.is_open())  {
-        std::cerr << "Error opening output file." << std::endl;
-        return 1;
-    }
+     // Get current directory (PAM4)
+     std::filesystem::path current_dir = std::filesystem::current_path();
 
-    std::ofstream outputFile2("prbs7buff.txt");
-    if (!outputFile.is_open())  {
-        std::cerr << "Error opening output file." << std::endl;
-        return 1;
-    }
+     // Go up one level to lbl/, then into output/
+     std::filesystem::path output_path = current_dir.parent_path() / "output" / "prbs7.txt";
+ 
+     // Create output/ directory if it doesn't exist
+     std::filesystem::create_directories(output_path.parent_path());
+ 
+     // Open file for writing
+     std::ofstream outputFile(output_path);
+     if (!outputFile.is_open()) {
+         std::cerr << "Failed to open: " << output_path << std::endl;
+         return 1;
+     }
 
 
     const int bitCount = 8;
@@ -36,9 +41,15 @@ int main() {
             outBuffer[i] = outBuffer[i - 1];
         }
 
-        // write out_msb
+        // write out
         outputFile << outBuffer[2] << bitArr[7];
-        outputFile2 << outBuffer[2];
+
+        // just buffer prbs7 out
+        // outputFile2 << outBuffer[2];
+
+        // just prbs7 out
+        // outputFile2 << outBuffer[7];
+
         // xor bit 1 and 6 to get new bit 0
         int newBit = bitArr[1] ^ bitArr[7];
         int newBitBuff = bitArr[2] ^ bitArr[4];
